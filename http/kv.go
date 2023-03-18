c6d475699c66a9b551a76e7ae2ba8b15a809f199
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-03-17 20:28:55 -0700

    Key value DB and api challenge

--- http/kv.go
+++ http/kv.go
@@ -1,25 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
+const apiBase = "http://localhost:8080/kv"
+
 func list() error {
-	// FIXME
+	resp, err := http.Get(apiBase)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	defer resp.Body.Close()
+	var keys []string
+	if json.NewDecoder(resp.Body).Decode(&keys); err != nil {
+		return err
+	}
+
+	for _, key := range keys {
+		fmt.Println(key)
+	}
+
 	return nil
 }
 
 func set(key string) error {
-	// FIXME
+	url := fmt.Sprintf("%s/%s", apiBase, key)
+	resp, err := http.Post(url, "application/octet-stream", os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	var reply struct {
+		Key  string
+		Size int
+	}
+
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&reply); err != nil {
+		return err
+	}
+
+	fmt.Printf("%s: %d bytes\n", reply.Key, reply.Size)
 	return nil
 }
 
 func get(key string) error {
-	// FIXME
-	return nil
+	url := fmt.Sprintf("%s/%s", apiBase, key)
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	return err
 }
 
 func main() {
--- http/kvd.go
+++ http/kvd.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
 
 	"github.com/gorilla/mux"
+)
+
+const (
+	maxSize = 10 * (1 << 20) // 10MB
 )
 
 var (
@@ -14,20 +22,71 @@ var (
 )
 
 func handleSet(w http.ResponseWriter, r *http.Request) {
-	// FIXME
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	defer r.Body.Close()
+	rdr := io.LimitReader(r.Body, maxSize)
+	data, err := ioutil.ReadAll(rdr)
+	if err != nil {
+		log.Printf("read error: %s", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dbLock.Lock()
+	defer dbLock.Unlock()
+	database[key] = data
+
+	resp := map[string]interface{}{
+		"key":  key,
+		"size": len(data),
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Printf("error sending: %s", err)
+	}
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
-	// FIXME
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	dbLock.RLock()
+	defer dbLock.RUnlock()
+
+	data, ok := database[key]
+	if !ok {
+		log.Printf("error get - unknown key: %q", key)
+		http.Error(w, fmt.Sprintf("%q not found", key), http.StatusNotFound)
+		return
+	}
+
+	if _, err := w.Write(data); err != nil {
+		log.Printf("error sending: %s", err)
+	}
 }
 
 func handleList(w http.ResponseWriter, r *http.Request) {
-	// FIXME
+	dbLock.RLock()
+	defer dbLock.RUnlock()
+
+	keys := make([]string, 0, len(database))
+	for key := range database {
+		keys = append(keys, key)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(keys); err != nil {
+		log.Printf("error sending: %s", err)
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	// FIXME: routing
+	r.HandleFunc("/kv/{key}", handleSet).Methods("POST")
+	r.HandleFunc("/kv/{key}", handleGet).Methods("GET")
+	r.HandleFunc("/kv", handleList).Methods("GET")
 	http.Handle("/", r)
 
 	addr := ":8080"
@@ -35,4 +94,10 @@ func main() {
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
+
+	// curl --header "Content-Type: application/json" -d "{\"testing\":\"bark\"}" http://localhost:8080/kv/dog
+	// curl --header "Content-Type: application/json" -d "{\"pesting\":\"meow\"}" http://localhost:8080/kv/cat
+	// curl http://localhost:8080/kv
+	// curl http://localhost:8080/kv/dog
+	// curl http://localhost:8080/kv/cat
 }
