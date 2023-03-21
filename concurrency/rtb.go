27487f7da19c80d3399d5ba6bf96cbbce9431bf8
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-03-21 15:03:05 -0700

    Real time bidding example with timeout

---
+++ concurrency/rtb.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+var state = 0
+
+func bestBid(url string) Bid {
+	state = 1 - state // toggle state
+	if state == 1 {
+		time.Sleep(2 * time.Millisecond)
+		return Bid{
+			Price: 0.035,
+			URL:   url,
+		}
+	}
+
+	time.Sleep(bidTimeout + 20*time.Millisecond)
+	return Bid{
+		Price: 0.018,
+		URL:   url,
+	}
+
+}
+
+type Bid struct {
+	Price float64
+	URL   string
+}
+
+var (
+	defaultBid = Bid{
+		Price: 0.02,
+		URL:   "https://j.mp/3cbDsIY",
+	}
+	bidTimeout = 10 * time.Millisecond
+)
+
+func bidOn(ctx context.Context, url string) Bid {
+	ch := make(chan Bid, 1)
+	go func() {
+		ch <- bestBid(url)
+	}()
+
+	select {
+	case bid := <-ch:
+		return bid
+	case <-ctx.Done():
+		log.Printf("bid for %q timed out, returning default", url)
+		return defaultBid
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), bidTimeout)
+	defer cancel()
+	bid := bidOn(ctx, "https://353solutions.com")
+	fmt.Println(bid)
+	// {0.035 https://j.mp/3f3Dpkb}
+
+	ctx, cancel = context.WithTimeout(context.Background(), bidTimeout)
+	defer cancel()
+	bid = bidOn(ctx, "https://example.com")
+	fmt.Println(bid)
+	// 2021/05/02 19:10:00 bid for "https://example.com" timed out, returning default
+	// {0.02 https://j.mp/3cbDsIY}
+}
